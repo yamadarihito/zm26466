78fd20d36dd89ab4010afd832415930434cac95e
yamadarihito <zm26466@josai.ac.jp>
2026-07-22 22:27:23 +0900

    Update 期末プロジェクトmyjs.js

--- 期末プロジェクトmyjs.js
+++ 期末プロジェクトmyjs.js
@@ -1,1 +1,39 @@
+body{
 
+    background:#eef7ff;
+
+    text-align:center;
+
+    font-family:sans-serif;
+
+}
+
+h1{
+
+    color:navy;
+
+}
+
+img{
+
+    width:300px;
+
+    border-radius:15px;
+
+}
+
+a{
+
+    color:red;
+
+    font-size:20px;
+
+    text-decoration:none;
+
+}
+
+a:hover{
+
+    color:green;
+
+}
